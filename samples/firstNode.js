26ea2a36ab09ee45766a854a1eb0ec1e5194ca12
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-03-10 16:00:03 +0200

    Update firstNode.js

--- samples/firstNode.js
+++ samples/firstNode.js
@@ -11,6 +11,16 @@ console.warn("Warning!");           // => Warning!
 
 console.log(process.argv);    // => [ 'C:\\Program Files\\nodejs\\node.exe' ]
 
+console.log(process.env); 
+console.log(process.env["HOME"]);
 
+const args = process.argv.slice(2);
+console.log(process.env[args[0]]);
+
+args.forEach(arg => {
+  let envVar = process.env[arg];
+  if (envVar === undefined) { console.error(`Could not find "${arg}" in environment`);
+  } else { console.log(envVar); }
+});
 
 
