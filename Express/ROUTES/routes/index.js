5c0cfeef6d87e1f53a0deaf3e400a26f32e5da58
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-10-07 13:23:05 +0300

    style: enhance code comments
    update index.js

--- Express/ROUTES/routes/index.js
+++ Express/ROUTES/routes/index.js
@@ -1,3 +1,8 @@
+// index.js
+/* 
+  This file serves as the main router for your app, 
+  aggregating routes from other files.
+*/
 const express = require('express');
 const commentsRouter = require('./comments');
 const usersRouter = require('./users');
@@ -5,6 +10,10 @@ const rootRouter = require('./root');
 
 const router = express.Router();
 
+/*
+  We use router.use() to mount routers under their respective paths
+   ("/comments", "/users", and "/") to create a hierarchical route structure.
+*/
 router.use('/comments', commentsRouter);
 router.use('/users', usersRouter);
 router.use('/', rootRouter);
